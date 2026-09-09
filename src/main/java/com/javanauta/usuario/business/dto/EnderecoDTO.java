f90444260a77bbf473c3953a0647b6fd9293547e
gabrielmendesdev7 <andrewsseven@gmail.com>
2026-09-09 17:58:21 -0300

    adicionando método que salva dados usuário

---
+++ src/main/java/com/javanauta/usuario/business/dto/EnderecoDTO.java
@@ -0,0 +1,24 @@
+package com.javanauta.usuario.business.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EnderecoDTO {
+
+    private String rua;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String estado;
+    private String cep;
+
+}
